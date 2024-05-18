2234438d7b380482a165bd3e8ab54294eeab9e02
Your Name <you@example.com>
2024-05-17 20:14:08 -0700

    fixed chat ids

--- src/utils/generateChatId.js
+++ src/utils/generateChatId.js
@@ -1,5 +1,12 @@
 export const generateChatId = (doctorId, userName) => {
+  // Ensure "Dr " is at the beginning of userName if it's not already there
+  const formattedUserName = userName.startsWith('Dr') ? userName : `Dr ${userName}`;
+
+  // Add a space before the userName
+  const formattedUserNameWithSpace = ` ${formattedUserName}`;
+
   // Remove invalid characters from userName
-  const sanitizedUserName = userName.replace(/[.#$[\]]/g, '');
+  const sanitizedUserName = formattedUserNameWithSpace.replace(/[.#$[\]]/g, '');
+
   return `${doctorId}${sanitizedUserName}`;
 };
