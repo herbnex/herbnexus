1fa6b4d9a7fb7503704b44cabc153a2fd00b5eb3
Your Name <you@example.com>
2024-05-25 17:58:54 -0700

    firebase subscription edit

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,34 +1,8 @@
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { initializeApp } = require('firebase/app');
-const { getFirestore, doc, updateDoc } = require('firebase/firestore');
-
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
 
 exports.handler = async (event, context) => {
-  console.log('Function invoked');
-  console.log('Environment Variables:', {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-  });
-
   const { userId } = JSON.parse(event.body);
-  console.log('Received userId:', userId);
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
@@ -40,13 +14,6 @@ exports.handler = async (event, context) => {
       metadata: { userId },
     });
 
-    console.log('Payment Intent created:', paymentIntent);
-
-    // Update Firestore to mark the user as subscribed
-    await updateDoc(doc(db, 'users', userId), {
-      isSubscribed: true,
-    });
-
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -54,7 +21,6 @@ exports.handler = async (event, context) => {
       }),
     };
   } catch (error) {
-    console.error('Error creating payment intent:', error.message);
     return {
       statusCode: 400,
       body: JSON.stringify({ error: error.message }),
