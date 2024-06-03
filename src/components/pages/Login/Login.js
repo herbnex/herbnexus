6ecd63c247ae3f8c4e3db23910380af6e4738c96
Your Name <you@example.com>
2024-06-02 19:05:18 -0700

    undo modals 1

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Form, Row, Button, FloatingLabel, Alert, Spinner, Modal } from "react-bootstrap";
+import { Col, Container, Form, Row, Button, FloatingLabel, Alert, Spinner } from "react-bootstrap";
 import { NavLink, useLocation, useHistory } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 import Error from "../../Error/Error";
@@ -12,7 +12,6 @@ const Login = () => {
   const [validationErrors, setValidationErrors] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [loginError, setLoginError] = useState("");
-  const [show, setShow] = useState(true);
   const history = useHistory();
   const location = useLocation();
 
@@ -45,63 +44,69 @@ const Login = () => {
     }
   }, [user, history, refferer]);
 
-  const handleClose = () => setShow(false);
-
   return (
-    <Modal show={show} onHide={handleClose} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Login</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
+    <div>
+      <Container fluid className="login-heading">
         {authError && <Error />}
-        {loginError && <Alert variant="danger">{loginError}</Alert>}
-        <Form onSubmit={handleLoginSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
-              <Form.Control
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="rounded-pill ps-4"
-                type="email"
-                placeholder="name@example.com"
-                isInvalid={!!validationErrors.email}
-                required
-              />
-              <Form.Control.Feedback type="invalid">{validationErrors.email}</Form.Control.Feedback>
-            </FloatingLabel>
-          </Form.Group>
+      </Container>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <FloatingLabel controlId="floatingPassword" label="Password">
-              <Form.Control
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="rounded-pill ps-4"
-                type="password"
-                placeholder="Password"
-                isInvalid={!!validationErrors.password}
-                required
-              />
-              <Form.Control.Feedback type="invalid">{validationErrors.password}</Form.Control.Feedback>
-            </FloatingLabel>
-          </Form.Group>
+      <Container className="login-panel">
+        <Row>
+          <Col xs={12} md={6}>
+            <h1 className="title text-center">Login</h1>
+            <div className="login d-flex flex-column justify-content-center h-100 pb-5">
+              {loginError && <Alert variant="danger">{loginError}</Alert>}
+              <Form onSubmit={handleLoginSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+                    <Form.Control
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="rounded-pill ps-4"
+                      type="email"
+                      placeholder="name@example.com"
+                      isInvalid={!!validationErrors.email}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">{validationErrors.email}</Form.Control.Feedback>
+                  </FloatingLabel>
+                </Form.Group>
 
-          <Button variant="outline" className="btn-main rounded-pill p-3 w-100" type="submit" disabled={isLoading}>
-            {isLoading ? (
-              <>
-                <Spinner animation="border" size="sm" className="me-2" />
-                Signing in...
-              </>
-            ) : (
-              "Log In"
-            )}
-          </Button>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <NavLink to={{ pathname: "/signup", state: { from: refferer } }}>Sign Up</NavLink>
-      </Modal.Footer>
-    </Modal>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <FloatingLabel controlId="floatingPassword" label="Password">
+                    <Form.Control
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="rounded-pill ps-4"
+                      type="password"
+                      placeholder="Password"
+                      isInvalid={!!validationErrors.password}
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">{validationErrors.password}</Form.Control.Feedback>
+                  </FloatingLabel>
+                </Form.Group>
+
+                <Button variant="outline" className="btn-main rounded-pill p-3 w-100" type="submit" disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Spinner animation="border" size="sm" className="me-2" />
+                      Signing in...
+                    </>
+                  ) : (
+                    "Log In"
+                  )}
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+
+        <h6 className="my-5 pt-5 text-center">
+          No Account yet? <NavLink to={{ pathname: "/signup", state: { from: refferer } }}>Sign Up</NavLink> instead.
+        </h6>
+      </Container>
+    </div>
   );
 };
 
