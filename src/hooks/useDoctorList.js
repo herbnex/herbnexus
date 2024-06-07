41f5afc064ce058f27c1d6d1d7a282ddeba347a8
Your Name <you@example.com>
2024-06-07 12:34:01 -0700

    modified usedoctorlist logic to get dr from db instead of local json

--- src/hooks/useDoctorList.js
+++ src/hooks/useDoctorList.js
@@ -1,12 +1,26 @@
 import { useEffect, useState } from "react";
+import { db } from "../../src/Firebase/firebase.config"; // Adjust the path as necessary
+import { collection, getDocs } from "firebase/firestore";
 
 const useDoctorList = () => {
   const [doctors, setDoctors] = useState([]);
 
   useEffect(() => {
-    fetch("/doctorsList.json")
-      .then((res) => res.json())
-      .then((data) => setDoctors(data));
+    const fetchDoctors = async () => {
+      try {
+        const doctorsCollection = collection(db, "doctors");
+        const doctorsSnapshot = await getDocs(doctorsCollection);
+        const doctorsList = doctorsSnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setDoctors(doctorsList);
+      } catch (error) {
+        console.error("Error fetching doctors: ", error);
+      }
+    };
+
+    fetchDoctors();
   }, []);
 
   return [doctors, setDoctors];
