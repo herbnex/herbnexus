05cdb0878ea1a3b2dec1404ef44d879eeba99b6b
Your Name <you@example.com>
2024-06-12 03:58:40 -0700

    shop promo using bootsrap instead of slick for slider

--- src/components/ShopPromo/ShopPromo.js
+++ src/components/ShopPromo/ShopPromo.js
@@ -34,13 +34,13 @@ const ShopPromo = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
           dots: true
@@ -49,17 +49,11 @@ const ShopPromo = () => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
       }
+      
     ]
   };
 
