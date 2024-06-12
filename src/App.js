2cb7173d7cd80d8ac0f1dbb5766999a94d3cd72c
Your Name <you@example.com>
2024-06-12 15:29:59 -0700

    implement scroll hook in app.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     <div className="App">
       <AuthProvider>
         <NavBar />
-        <ScrollToTop />
+        
         <Switch>
           <Route exact path="/">
             <Home />
