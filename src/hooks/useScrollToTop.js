655b6fd89ee6bf094fa05ad232a2806588f097d6
Your Name <you@example.com>
2024-06-12 16:22:09 -0700

    implement scroll hook in app.js

--- src/hooks/useScrollToTop.js
+++ src/hooks/useScrollToTop.js
@@ -1,37 +1,30 @@
-import { useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const useScrollToTop = () => {
   const location = useLocation();
 
   useLayoutEffect(() => {
+    const savedPosition = sessionStorage.getItem('scrollPosition');
+    if (!sessionStorage.getItem('visitedBefore')) {
+      window.scrollTo(0, 0);
+      sessionStorage.setItem('visitedBefore', 'true');
+    } else if (savedPosition) {
+      window.scrollTo(0, parseInt(savedPosition, 10));
+    }
+  }, [location.pathname]);
+
+  useEffect(() => {
     const handleScroll = () => {
       sessionStorage.setItem('scrollPosition', window.scrollY);
     };
 
-    const handleBeforeUnload = () => {
-      sessionStorage.setItem('scrollPosition', window.scrollY);
-    };
-
     window.addEventListener('scroll', handleScroll);
-    window.addEventListener('beforeunload', handleBeforeUnload);
-
-    // Check if the user has visited the page before
-    if (!sessionStorage.getItem('visitedBefore')) {
-      window.scrollTo(0, 0);
-      sessionStorage.setItem('visitedBefore', 'true');
-    } else {
-      const savedPosition = sessionStorage.getItem('scrollPosition');
-      if (savedPosition) {
-        window.scrollTo(0, parseInt(savedPosition, 10));
-      }
-    }
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('beforeunload', handleBeforeUnload);
     };
-  }, [location.pathname]);
+  }, []);
 };
 
 export default useScrollToTop;
