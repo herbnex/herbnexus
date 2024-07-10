f1f2de6d87403285dd439a87bc8aa8666ad92658
Your Name <you@example.com>
2024-07-10 05:47:08 -0700

    mal jet and appoiintemnt 1

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,46 +1,47 @@
-const mailjet = require('node-mailjet').connect(
-    process.env.MJ_API_KEY,
-    process.env.MJ_SECRET_KEY
-  );
-  
-  exports.handler = async (event, context) => {
-    const { userEmail, doctorEmail, subject, message } = JSON.parse(event.body);
-  
-    const request = mailjet.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: {
-            Email: 'noreply@herbnexus.com',
-            Name: 'Herb Nexus',
-          },
-          To: [
-            {
-              Email: userEmail,
-              Name: 'User',
-            },
-            {
-              Email: doctorEmail,
-              Name: 'Doctor',
-            },
-          ],
-          Subject: subject,
-          TextPart: message,
-          HTMLPart: `<p>${message}</p>`,
+const mailjet = require('node-mailjet');
+
+const client = mailjet.apiConnect(
+  process.env.MJ_APIKEY_PUBLIC,
+  process.env.MJ_APIKEY_PRIVATE
+);
+
+exports.handler = async (event, context) => {
+  const { userEmail, doctorEmail, subject, message } = JSON.parse(event.body);
+
+  const request = client.post('send', { version: 'v3.1' }).request({
+    Messages: [
+      {
+        From: {
+          Email: 'noreply@herbnexus.com',
+          Name: 'Herb Nexus',
         },
-      ],
-    });
-  
-    try {
-      const result = await request;
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ result }),
-      };
-    } catch (err) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: err.message }),
-      };
-    }
-  };
-  
+        To: [
+          {
+            Email: userEmail,
+            Name: 'User',
+          },
+          {
+            Email: doctorEmail,
+            Name: 'Doctor',
+          },
+        ],
+        Subject: subject,
+        TextPart: message,
+        HTMLPart: `<p>${message}</p>`,
+      },
+    ],
+  });
+
+  try {
+    const result = await request;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ result }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
