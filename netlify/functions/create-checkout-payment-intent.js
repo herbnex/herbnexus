fda08bad035734f960c199f7aedc419031e97460
Your Name <you@example.com>
2024-07-12 01:40:45 -0700

    implemented address in serverless checkout payemnt intent and made address in shipping in strupe form

--- netlify/functions/create-checkout-payment-intent.js
+++ netlify/functions/create-checkout-payment-intent.js
@@ -4,17 +4,29 @@ const { db } = require("../../src/Firebase/setupFirebaseAdmin");
 
 exports.handler = async (event) => {
   try {
-    const { cart } = JSON.parse(event.body);
-        // Calculate the total amount for the cart
+    const { cart, shippingAddress } = JSON.parse(event.body);
+
+    // Calculate the total amount for the cart
     const totalAmount = Math.round(cart.reduce((sum, item) => sum + item.price * item.quantity, 0) * 100); // Amount in cents
 
-    // Create a payment intent
+    // Create a payment intent with shipping details
     const paymentIntent = await stripe.paymentIntents.create({
       amount: totalAmount,
       currency: 'cad',
+      shipping: {
+        name: shippingAddress.recipient,
+        address: {
+          line1: shippingAddress.addressLine,
+          city: shippingAddress.city,
+          state: shippingAddress.region,
+          postal_code: shippingAddress.postalCode,
+          country: shippingAddress.country,
+        },
+        phone: shippingAddress.phone,
+      },
     });
 
-        return {
+    return {
       statusCode: 200,
       body: JSON.stringify({
         clientSecret: paymentIntent.client_secret,
