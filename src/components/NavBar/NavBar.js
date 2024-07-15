8fbe18b913f4954b008272fae1f1ca219341802b
Your Name <you@example.com>
2024-07-14 22:25:19 -0700

    navbar reset because dashabrd wasnt showing on mobile commnted out logout from mobile reset consditpornaly render logout iin mobile added classname to dash

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -113,7 +113,7 @@ const NavBar = () => {
 								</div>
 							)}
 							<div className="mt-2 mobile-profile flex-column align-items-center gap-4">
-							<NavLink to="/dashboard">Dashboard</NavLink>
+							<NavLink class="nav-item"to="/dashboard">Dashboard</NavLink>
 							{user && (
 								<Button onClick={logOut} variant="danger" className="rounded-pill mx-3 p-2 nav-small-text px-3">
 									Log Out&nbsp;
